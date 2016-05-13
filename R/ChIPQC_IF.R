b74ceb5accab048b0bcc8754d151ba14223231ef
Tom Carroll <tc.infomatics@gmail.com>
2016-05-13 13:19:32 +0000

    Fixes to sampleQC and verbosity config
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/branches/RELEASE_3_3/madman/Rpacks/ChIPQC@117449 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/ChIPQC_IF.R
+++ R/ChIPQC_IF.R
@@ -1,11 +1,11 @@
-ChIPQCsample = function(reads, peaks, annotation, chromosomes=NULL, 
-                        mapQCth=15, blacklist, profileWin=400,fragmentLength=125,shifts=1:300,runCrossCor=FALSE) {
+ChIPQCsample = function(reads, peaks, annotation=NULL, chromosomes=NULL, 
+                        mapQCth=15, blacklist, profileWin=400,fragmentLength=125,shifts=1:300,runCrossCor=FALSE,verboseT=TRUE) {
    
    if(missing(peaks)) peaks=NULL
    if(missing(blacklist)) blacklist=NULL
    res = sampleQC(bamFile=reads, bedFile=peaks, GeneAnnotation=annotation,blklist=blacklist,ChrOfInterest=chromosomes,
                   Window=profileWin,FragmentLength=fragmentLength,
-                  shiftWindowStart=min(shifts),shiftWindowEnd=max(shifts),runCrossCor=runCrossCor)
+                  shiftWindowStart=min(shifts),shiftWindowEnd=max(shifts),runCrossCor=runCrossCor,verboseT=verboseT)
    
    return(res)
 }
